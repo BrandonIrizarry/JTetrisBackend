27f699c72593345e74036531cedfe9dc3746c8d9
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-08-12 19:24:26 -0400

    Print empty board

--- src/tetris/Main.java
+++ src/tetris/Main.java
@@ -1,66 +1,27 @@
 package tetris;
 
-import java.util.Arrays;
+import java.util.Map;
 import java.util.Scanner;
 
-final class Board {
-    private final int width;
-    private final int height;
-    private final char[][] cells;
-    private static final char DEFAULT_CHAR = '-';
-    private static final char PIECE_CHAR = '0';
-
-    public Board(int width, int height) {
-        this.cells = new char[width][height];
-
-        // Initialize all cells to '-'
-        for (var row : cells) {
-            Arrays.fill(row, DEFAULT_CHAR);
-        }
-
-        this.width = width;
-        this.height = height;
-    }
-
-    private void updateState(Piece piece) {
-        var drawingTemplate = piece.getDrawingTemplate(width);
-
-        for (int row = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
-                var linearIndex = col + width * row;
-
-                if (drawingTemplate.contains(linearIndex)) {
-                    cells[row][col] = PIECE_CHAR;
-                } else {
-                    cells[row][col] = DEFAULT_CHAR;
-                }
-            }
-        }
-    }
-
-    public String getStringRepresentation(Piece piece) {
-        updateState(piece);
-        var builder = new StringBuilder();
-
-        for (var row : cells) {
-            for (var cell : row) {
-                builder.append("%c ".formatted(cell));
-            }
-
-            builder.append("%n");
-        }
-
-        return builder.toString();
-    }
-}
-
-/** TODO
- *     Allow updateState to accept a null parameter, in which case the drawing template is set to -1, -1, -1, -1,
- *     so that '-' is written everywhere in the board. (We can have that bogus drawing template saved somewhere
- *     as a static field, either in a Utils class, or something similar.)
- */
 public class Main {
+    final static Map<String, Piece> pieceTable = Map.of(
+            "I", Pieces.II,
+            "J", Pieces.JJ,
+            "L", Pieces.LL,
+            "O", Pieces.OO,
+            "S", Pieces.SS,
+            "T", Pieces.TT,
+            "Z", Pieces.ZZ
+    );
+
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
+        var pieceLetter = scanner.nextLine();
+        var boardWidth = scanner.nextInt();
+        var boardHeight = scanner.nextInt();
+
+        var board = new Board(boardHeight, boardWidth);
+        board.updateState(null);
+        System.out.println(board);
     }
 }
