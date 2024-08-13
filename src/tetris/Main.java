6b89663faa1066461434f54af7a3a2340dbbb89c
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-08-12 21:41:34 -0400

    WIP: trying different drop+move configurations
    It looks like I need to revise some of the piece data! I didn't notice
    the exercise was starting to use different numbers.

--- src/tetris/Main.java
+++ src/tetris/Main.java
@@ -1,5 +1,7 @@
 package tetris;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -21,8 +23,14 @@ public class Main {
         var piece = pieceTable.get(pieceLetter);
         if (piece == null) throw new AssertionError();
 
-        var boardWidth = scanner.nextInt();
-        var boardHeight = scanner.nextInt();
+        // We can't simply use 'nextInt', since this doesn't consume the newline. This omission
+        // later trips up the 'scanner.nextLine()' invocation when reading game commands.
+        List<Integer> boardDimensions = Arrays.stream(scanner.nextLine().split("\\s+"))
+                                                .map(Integer::parseInt)
+                                                .toList();
+
+        var boardWidth = boardDimensions.get(0);
+        var boardHeight = boardDimensions.get(1);
 
         var board = new Board(boardHeight, boardWidth);
         board.updateState(null);
@@ -31,5 +39,29 @@ public class Main {
         piece.setFrameOrigin(3, 0);
         board.updateState(piece);
         System.out.println(board);
+
+        // game command loop
+        gameLoop:
+        while (true) {
+            var command = scanner.nextLine();
+
+            switch(command) {
+                case "rotate" -> piece.rotateCounterclockwise();
+                case "left" -> {
+                    piece.moveLeft();
+                    piece.moveDown();
+                }
+                case "right" -> {
+                    piece.moveRight();
+                    piece.moveDown();
+                }
+                case "down" -> piece.moveDown();
+                case "exit" -> { break gameLoop; }
+                default -> throw new IllegalArgumentException("invalid command");
+            }
+
+            board.updateState(piece);
+            System.out.println(board);
+        }
     }
 }
