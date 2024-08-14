382eb4d6cf47369342d33a641ae7863221d0d200
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-08-13 20:16:37 -0400

    WIP: correctly implement right-side barrier
    However, the left barrier blocks the 0th column from being used, which
    I'll fix next.

--- src/tetris/Piece.java
+++ src/tetris/Piece.java
@@ -1,9 +1,11 @@
 package tetris;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
+
+enum Move {
+    Down, Left, Right, Clockwise, Counterclockwise
+}
 
 /** The internal data representation of a Tetris piece.
  * The successive elements of the 'rotations' buffer describe
@@ -13,7 +15,19 @@ public final class Piece {
     private final Deque<List<Integer>> rotations = new ArrayDeque<>();
     private int xOffset;
     private int yOffset;
+
+    private final static Map<Move, Consumer<Piece>> undoActions = Map.of(
+            Move.Down, Piece::moveDown,
+            Move.Left, Piece::moveRight,
+            Move.Right, Piece::moveLeft,
+            Move.Clockwise, Piece::rotateCounterclockwise,
+            Move.Counterclockwise, Piece::rotateClockwise
+    );
+
+    private Move lastMove;
+
     public static final int FRAME_DIMENSION = 4;
+    public static final int NUM_TILES = 4;
 
     @SafeVarargs
     public Piece(List<Integer>... rotations) {
@@ -33,20 +47,24 @@ public final class Piece {
 
     public void moveLeft() {
         xOffset--;
+        lastMove = Move.Left;
     }
 
     public void moveRight() {
         xOffset++;
+        lastMove = Move.Right;
     }
 
     public void rotateCounterclockwise() {
         var tmp = rotations.pop();
         rotations.addLast(tmp);
+        lastMove = Move.Counterclockwise;
     }
 
     public void rotateClockwise() {
         var tmp = rotations.removeLast();
         rotations.push(tmp);
+        lastMove = Move.Clockwise;
     }
 
     public List<Integer> getCurrentRotation() {
@@ -54,22 +72,35 @@ public final class Piece {
     }
 
     public List<Integer> getDrawingTemplate(int boardWidth) {
-        var basePoint = xOffset + boardWidth * yOffset;
+        List<Integer> drawingTemplate = null;
 
-        return getCurrentRotation()
-                       .stream()
-                       .map(point -> {
-                           var rowBasePoint = basePoint + (point / FRAME_DIMENSION) * boardWidth;
-                           var finalPoint = rowBasePoint + point % FRAME_DIMENSION;
+        while (true) {
+            var basePoint = xOffset + boardWidth * yOffset;
 
-                           // Detect whether this point has wrapped around to the next row;
-                           // if so, bring it back up to (x, y - 1).
-                           if (finalPoint % boardWidth < rowBasePoint % boardWidth) {
-                               finalPoint -= boardWidth;
-                           }
+            drawingTemplate = getCurrentRotation()
+                                      .stream()
+                                      .takeWhile(point -> {
+                                          var rowBasePoint = basePoint + (point / FRAME_DIMENSION) * boardWidth;
+                                          var finalPoint = rowBasePoint + point % FRAME_DIMENSION;
 
-                           return finalPoint;
-                       })
-                       .toList();
+                                          return finalPoint % boardWidth >= rowBasePoint % boardWidth;
+                                      })
+                                      .map(point -> {
+                                          var rowBasePoint = basePoint + (point / FRAME_DIMENSION) * boardWidth;
+
+                                          return rowBasePoint + point % FRAME_DIMENSION;
+                                      })
+                                      .toList();
+
+            if (drawingTemplate.size() == NUM_TILES) {
+                break;
+            } else {
+                var undoMethod = undoActions.get(lastMove);
+
+                undoMethod.accept(this);
+            }
+        }
+
+        return drawingTemplate;
     }
 }
