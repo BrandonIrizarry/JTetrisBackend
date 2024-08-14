9e757aa4b8bdf62903a045cb8776ba297d2bd579
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-08-13 22:46:43 -0400

    Switch order of freezing and validation
    This code also passes stage 3.

--- src/tetris/Board.java
+++ src/tetris/Board.java
@@ -32,17 +32,18 @@ public final class Board {
 
         var drawingTemplate = piece.getDrawingTemplate(width);
 
+        // Refetch a drawing template if the current one is invalid, undoing the piece's
+        // previous move. Note that we have to perform this code before any freezing, or else
+        // the undo mechanism inside this if-block won't take effect.
+        if (!validate(drawingTemplate)) {
+            piece.undo();
+            drawingTemplate = piece.getDrawingTemplate(width);
+        }
+
         // If any of the drawing template's points is at the bottom row of the board,
         // the piece has officially landed, and so can't be moved anymore.
         if (drawingTemplate.stream().anyMatch(point -> point / width == height - 1)) {
             piece.freeze();
-        }
-
-        // Refetch a drawing template if the current one is invalid, undoing the piece's
-        // previous move.
-        if (!validate(drawingTemplate)) {
-            piece.undo();
-            drawingTemplate = piece.getDrawingTemplate(width);
         }
 
         // Update the board according to whether the drawing template specifies
