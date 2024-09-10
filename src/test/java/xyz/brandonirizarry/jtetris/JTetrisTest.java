8cf75f5d3a4a7ef16e2b1ab5a8c269502028d9b8
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-09-10 19:00:56 -0400

    Setup Gradle build code
    This includes testing facilities.
    
    To this end, I've also added a dummy, proof-of-concept test to prove
    everything works (I'm currently still somewhat of a beginner when it
    comes to using Gradle.)
    
    Hopefully, the Gradle setup I've designed so far should outline our
    basic project organization strategy.

---
+++ src/test/java/xyz/brandonirizarry/jtetris/JTetrisTest.java
@@ -0,0 +1,12 @@
+package xyz.brandonirizarry.jtetris;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JTetrisTest {
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
