112f08d1a43680e5293d91d63e2a06d226c9ba6f
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-09-10 22:17:13 -0400

    Implement Z-piece template with test
    This merely consists of adding the 'Z' directory as a project
    resource. This directory contains the successive rotations of the 'Z'
    piece.
    
    The successive (counterclockwise) rotations of any piece should be
    processed in lexicographic order of the corresponding filename. In
    practice, this corresponds to the first rotation (for example, of 'Z')
    corresponding to 'Z/1.txt', the second to 'Z/2.txt', and so on.
    
    The test added in this commit (our first test!) checks whether the
    code reading the piece from the filename maps to the correct
    coordinates.

--- src/main/java/xyz/brandonirizarry/jtetris/JTetris.java
+++ src/main/java/xyz/brandonirizarry/jtetris/JTetris.java
@@ -1,7 +1,95 @@
 package xyz.brandonirizarry.jtetris;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+enum GridToken {
+    Empty('-'), Piece('O');
+
+    private final char symbol;
+
+    GridToken(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+}
+
+record Coordinate(int row, int column) { }
+
 public class JTetris {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static Map<String, Deque<List<Coordinate>>> pieceMap = new HashMap<>(Map.of(
+            /*
+            "I", new ArrayDeque<>(),
+            "J", new ArrayDeque<>(),
+            "L", new ArrayDeque<>(),
+            "O", new ArrayDeque<>(),
+            "S", new ArrayDeque<>(),
+            "T", new ArrayDeque<>(),
+             */
+            "Z", new ArrayDeque<>()
+    ));
+
+    public static void main(String[] args) throws URISyntaxException {
+        for (var pieceName : pieceMap.keySet()) {
+            var dir = JTetris.class.getClassLoader().getResource(pieceName);
+            assert dir != null;
+
+            var rotations = pieceMap.get(pieceName);
+
+            try (var directoryContentsStream = Files.list(Paths.get(dir.toURI()))) {
+                directoryContentsStream
+                        .sorted()
+                        .forEach(path -> {
+                    try (var scanner = new Scanner(path).useDelimiter("")) {
+                        var rowIndex = 0;
+                        var columnIndex = 0;
+                        List<Coordinate> rotation = new ArrayList<>();
+
+                        while (scanner.hasNext()) {
+                            var rawToken = scanner.next().charAt(0);
+
+                            if (rawToken == '\n') {
+                                continue;
+                            }
+
+                            if (rawToken == 'O') {
+                                rotation.add(new Coordinate(rowIndex, columnIndex));
+                            }
+
+                            columnIndex++;
+
+                            if (columnIndex == 4) {
+                                columnIndex = 0;
+                                rowIndex++;
+
+                                if (rowIndex == 4) {
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (rowIndex > 4) {
+                            throw new IllegalStateException(
+                                    "rowIndex out of bounds with value: %d".formatted(rowIndex)
+                            );
+                        }
+
+                        rotations.add(rotation);
+                    } catch (IOException e) {
+                        System.out.println("what?");
+                    }
+                });
+            } catch (IOException e) {
+                System.out.println("oops");
+            }
+
+            pieceMap.put(pieceName, rotations);
+        }
     }
 }
--- src/test/java/xyz/brandonirizarry/jtetris/JTetrisTest.java
+++ src/test/java/xyz/brandonirizarry/jtetris/JTetrisTest.java
@@ -2,11 +2,37 @@ package xyz.brandonirizarry.jtetris;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JTetrisTest {
     @Test
-    void demoTestMethod() {
-        assertTrue(true);
+    void testZ() throws URISyntaxException {
+        JTetris.main(new String[]{});
+        var actualPieceZ = JTetris.pieceMap.get("Z")
+                                                .stream()
+                                                .flatMap(Collection::stream)
+                                                .toList();
+
+        var expectedPieceZ = List.of(
+                // First rotation.
+                new Coordinate(0, 1),
+                new Coordinate(0, 2),
+                new Coordinate(1, 2),
+                new Coordinate(1, 3),
+
+                // Second rotation.
+                new Coordinate(0, 2),
+                new Coordinate(1, 1),
+                new Coordinate(1, 2),
+                new Coordinate(2, 1)
+        );
+
+        for (var i = 0; i < expectedPieceZ.size(); i++) {
+            assertEquals(expectedPieceZ.get(i), actualPieceZ.get(i));
+        }
     }
 }
