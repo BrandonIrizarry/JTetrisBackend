d9e645c244d0fcb60ad64816ab02c41c80a90198
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-09-13 19:45:57 -0400

    test: check stencil coordinates for T

--- src/test/java/xyz/brandonirizarry/jtetris/JTetrisTest.java
+++ src/test/java/xyz/brandonirizarry/jtetris/JTetrisTest.java
@@ -40,4 +40,42 @@ public class JTetrisTest {
 
         assertEquals(expectedPieceZ, actualPieceZ, "Stencil generates incorrect coordinates");
     }
+
+    @Test
+    @DisplayName("T-piece stencil coordinates are correct")
+    void checkStencilCoordinatesT() {
+        var actualPieceT = JTetris.pieceMap.get("T");
+
+        var expectedPieceT = new CircularBuffer<>(
+                new Rotation(
+                        new Coordinate(0, 1),
+                        new Coordinate(1, 1),
+                        new Coordinate(1, 2),
+                        new Coordinate(2, 1)
+                ),
+
+                new Rotation(
+                        new Coordinate(0, 1),
+                        new Coordinate(1, 0),
+                        new Coordinate(1, 1),
+                        new Coordinate(1, 2)
+                ),
+
+                new Rotation(
+                        new Coordinate(0, 2),
+                        new Coordinate(1, 1),
+                        new Coordinate(1, 2),
+                        new Coordinate(2, 2)
+                ),
+
+                new Rotation(
+                        new Coordinate(0, 1),
+                        new Coordinate(0, 2),
+                        new Coordinate(0, 3),
+                        new Coordinate(1, 2)
+                )
+        );
+
+        assertEquals(expectedPieceT, actualPieceT, "Stencil generates incorrect coordinates");
+    }
 }
