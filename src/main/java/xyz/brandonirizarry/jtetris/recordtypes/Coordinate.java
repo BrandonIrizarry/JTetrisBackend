2bb55aa83196b7b0106ae75a9bb532021e56f9d5
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-09-15 19:28:40 -0400

    feat: implement spatial translation of Coordinate and Rotation
    This will certainly become important when I later implement falling
    pieces that are controllable by the user.

--- src/main/java/xyz/brandonirizarry/jtetris/recordtypes/Coordinate.java
+++ src/main/java/xyz/brandonirizarry/jtetris/recordtypes/Coordinate.java
@@ -1,6 +1,10 @@
 package xyz.brandonirizarry.jtetris.recordtypes;
 
 public record Coordinate(int row, int column) {
+    public Coordinate translate(int rowOffset, int columnOffset) {
+        return new Coordinate(row + rowOffset, column + columnOffset);
+    }
+
     @Override
     public String toString() {
         return "(%d %d)".formatted(row, column);
--- src/main/java/xyz/brandonirizarry/jtetris/recordtypes/Rotation.java
+++ src/main/java/xyz/brandonirizarry/jtetris/recordtypes/Rotation.java
@@ -22,4 +22,18 @@ public record Rotation(Coordinate a, Coordinate b, Coordinate c, Coordinate d) {
     public String toString() {
         return "<%s %s %s %s>".formatted(a, b, c, d);
     }
+
+    public Rotation translate(int rowOffset, int columnOffset) {
+        var aTranslated = a().translate(rowOffset, columnOffset);
+        var bTranslated = b().translate(rowOffset, columnOffset);
+        var cTranslated = c().translate(rowOffset, columnOffset);
+        var dTranslated = d().translate(rowOffset, columnOffset);
+
+        return new Rotation(
+                aTranslated,
+                bTranslated,
+                cTranslated,
+                dTranslated
+        );
+    }
 }
