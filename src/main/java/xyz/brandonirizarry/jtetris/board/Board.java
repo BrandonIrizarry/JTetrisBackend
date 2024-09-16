dab04e4da7b2b065b5752810eeeba2daf16f4015
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-09-16 13:01:15 -0400

    refactor: make Board::update use Rotation::hasMember

--- src/main/java/xyz/brandonirizarry/jtetris/board/Board.java
+++ src/main/java/xyz/brandonirizarry/jtetris/board/Board.java
@@ -44,17 +44,9 @@ public class Board {
     private void update() {
         var currentRotation = currentPiece.getFirst();
 
-        var a = currentRotation.a();
-        var b = currentRotation.b();
-        var c = currentRotation.c();
-        var d = currentRotation.d();
-
         for (var rowIndex = 0; rowIndex < board.length; rowIndex++) {
             for (var columnIndex = 0; columnIndex < board[rowIndex].length; columnIndex++) {
-                if (rowIndex == a.row() && columnIndex == a.column()
-                || rowIndex == b.row() && columnIndex == b.column()
-                || rowIndex == c.row() && columnIndex == c.column()
-                || rowIndex == d.row() && columnIndex == d.column()) {
+                if (currentRotation.hasMember(rowIndex, columnIndex)) {
                     board[rowIndex][columnIndex] = GridToken.Piece;
                 } else {
                     board[rowIndex][columnIndex] = GridToken.Empty;
