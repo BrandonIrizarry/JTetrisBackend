474c5745b47652df4dff39748118a8992b298c12
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-09-16 10:03:34 -0400

    test: displayed location of L-piece with no offsets

--- src/test/java/xyz/brandonirizarry/jtetris/board/BoardTest.java
+++ src/test/java/xyz/brandonirizarry/jtetris/board/BoardTest.java
@@ -3,6 +3,7 @@ package xyz.brandonirizarry.jtetris.board;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import xyz.brandonirizarry.jtetris.JTetris;
+import xyz.brandonirizarry.jtetris.Tetromino;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -29,6 +30,23 @@ public class BoardTest {
                             "Non-empty character");
                 }
             }
+        } catch (IOException e) {
+            System.out.println("Nonexistent file");
+        }
+    }
+
+    @Test
+    @DisplayName("Check display after introducing L")
+    void checkDisplayAfterIntroducingL() {
+        var board = new Board(20, 10);
+        board.introducePiece(Tetromino.L.getPiece());
+
+        try (var inStream = JTetris.class
+                                    .getClassLoader()
+                                    .getResourceAsStream("boardAfterIntroducingL.txt")) {
+            assert inStream != null;
+            var fileContents = new String(inStream.readAllBytes());
+            assertEquals(fileContents, board.toString());
         } catch (IOException e) {
             System.out.println("Nonexistent file");
         }
