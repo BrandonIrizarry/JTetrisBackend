678579d27c80ea279578c77a9b7c1976647701a8
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-09-15 20:09:22 -0400

    test: replace stencilCoordinateDefinitions map with Tetronimo enum
    This could potentially be a fertile idea for refactoring the main
    code. We'll see.

--- src/test/java/xyz/brandonirizarry/jtetris/StencilDefinitionsTest.java
+++ src/test/java/xyz/brandonirizarry/jtetris/StencilDefinitionsTest.java
@@ -2,177 +2,13 @@ package xyz.brandonirizarry.jtetris;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Named;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import xyz.brandonirizarry.jtetris.circularbuffer.CircularBuffer;
-import xyz.brandonirizarry.jtetris.circularbuffer.Piece;
-import xyz.brandonirizarry.jtetris.recordtypes.Coordinate;
-import xyz.brandonirizarry.jtetris.recordtypes.Rotation;
-
-import java.util.Map;
-import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.EnumSource;
+import xyz.brandonirizarry.jtetris.constants.Tetromino;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StencilDefinitionsTest {
-    private static final Map<String, CircularBuffer<Rotation>> stencilCoordinateDefinitions = Map.of(
-            "I", new Piece(
-                    new Rotation(
-                            new Coordinate(0, 0),
-                            new Coordinate(0, 1),
-                            new Coordinate(0, 2),
-                            new Coordinate(0, 3)
-                    ),
-
-                    new Rotation(
-                            new Coordinate(0, 1),
-                            new Coordinate(1, 1),
-                            new Coordinate(2, 1),
-                            new Coordinate(3, 1)
-                    )
-            ),
-            "J", new Piece(
-                    new Rotation(
-                            new Coordinate(0, 2),
-                            new Coordinate(1, 2),
-                            new Coordinate(2, 1),
-                            new Coordinate(2, 2)
-                    ),
-
-                    new Rotation(
-                            new Coordinate(0, 0),
-                            new Coordinate(0, 1),
-                            new Coordinate(0, 2),
-                            new Coordinate(1, 2)
-                    ),
-
-                    new Rotation(
-                            new Coordinate(0, 1),
-                            new Coordinate(0, 2),
-                            new Coordinate(1, 1),
-                            new Coordinate(2, 1)
-                    ),
-
-                    new Rotation(
-                            new Coordinate(0, 1),
-                            new Coordinate(1, 1),
-                            new Coordinate(1, 2),
-                            new Coordinate(1, 3)
-                    )
-            ),
-            "L", new Piece(
-                    new Rotation(
-                            new Coordinate(0, 1),
-                            new Coordinate(1, 1),
-                            new Coordinate(2, 1),
-                            new Coordinate(2, 2)
-                    ),
-
-                    new Rotation(
-                            new Coordinate(0, 2),
-                            new Coordinate(1, 0),
-                            new Coordinate(1, 1),
-                            new Coordinate(1, 2)
-                    ),
-
-                    new Rotation(
-                            new Coordinate(0, 1),
-                            new Coordinate(0, 2),
-                            new Coordinate(1, 2),
-                            new Coordinate(2, 2)
-                    ),
-
-                    new Rotation(
-                            new Coordinate(0, 1),
-                            new Coordinate(0, 2),
-                            new Coordinate(0, 3),
-                            new Coordinate(1, 1)
-                    )
-            ),
-            "O", new Piece(
-                    new Rotation(
-                            new Coordinate(0, 1),
-                            new Coordinate(0, 2),
-                            new Coordinate(1, 1),
-                            new Coordinate(1, 2)
-                    )
-            ),
-            "S", new Piece(
-                    new Rotation(
-                            new Coordinate(0, 1),
-                            new Coordinate(0, 2),
-                            new Coordinate(1, 0),
-                            new Coordinate(1, 1)
-                    ),
-
-                    new Rotation(
-                            new Coordinate(0, 1),
-                            new Coordinate(1, 1),
-                            new Coordinate(1, 2),
-                            new Coordinate(2, 2)
-                    )
-            ),
-            "T", new Piece(
-                    new Rotation(
-                            new Coordinate(0, 1),
-                            new Coordinate(1, 1),
-                            new Coordinate(1, 2),
-                            new Coordinate(2, 1)
-                    ),
-
-                    new Rotation(
-                            new Coordinate(0, 1),
-                            new Coordinate(1, 0),
-                            new Coordinate(1, 1),
-                            new Coordinate(1, 2)
-                    ),
-
-                    new Rotation(
-                            new Coordinate(0, 2),
-                            new Coordinate(1, 1),
-                            new Coordinate(1, 2),
-                            new Coordinate(2, 2)
-                    ),
-
-                    new Rotation(
-                            new Coordinate(0, 1),
-                            new Coordinate(0, 2),
-                            new Coordinate(0, 3),
-                            new Coordinate(1, 2)
-                    )
-            ),
-            "Z", new Piece(
-                    new Rotation(
-                            new Coordinate(0, 1),
-                            new Coordinate(0, 2),
-                            new Coordinate(1, 2),
-                            new Coordinate(1, 3)
-                    ),
-                    new Rotation(
-                            new Coordinate(0, 2),
-                            new Coordinate(1, 1),
-                            new Coordinate(1, 2),
-                            new Coordinate(2, 1)
-                    )
-            )
-    );
-
-    private static Stream<Arguments> getStencilTestArgs() {
-        return stencilCoordinateDefinitions.entrySet()
-                       .stream()
-                       .map(entry -> {
-                           var pieceName = entry.getKey();
-                           var stencilDefinition = entry.getValue();
-
-                           return Arguments.of(
-                                   Named.of("Piece %s".formatted(pieceName), pieceName),
-                                   stencilDefinition
-                           );
-                       });
-    }
-
     // Invoke 'main', so that the values of pieceMap are populated with
     // rotations.
     @BeforeAll
@@ -181,11 +17,11 @@ public class StencilDefinitionsTest {
     }
 
     @ParameterizedTest
-    @MethodSource("getStencilTestArgs")
+    @EnumSource
     @DisplayName("Stencil templates generate correct coordinates")
-    void checkStencilCoordinates(String pieceName, CircularBuffer<Rotation> stencilDefinition) {
-        var actualPiece = JTetris.pieceMap.get(pieceName);
+    void checkStencilCoordinates(Tetromino tetromino) {
+        var actualPiece = JTetris.pieceMap.get(tetromino.getLabel());
 
-        assertEquals(stencilDefinition, actualPiece, "Stencil generates incorrect coordinates");
+        assertEquals(tetromino.getPiece(), actualPiece, "Stencil generates incorrect coordinates");
     }
 }
---
+++ src/test/java/xyz/brandonirizarry/jtetris/constants/Tetromino.java
@@ -0,0 +1,159 @@
+package xyz.brandonirizarry.jtetris.constants;
+
+import xyz.brandonirizarry.jtetris.circularbuffer.Piece;
+import xyz.brandonirizarry.jtetris.recordtypes.Coordinate;
+import xyz.brandonirizarry.jtetris.recordtypes.Rotation;
+
+public enum Tetromino {
+    I("I", new Piece(
+            new Rotation(
+                    new Coordinate(0, 0),
+                    new Coordinate(0, 1),
+                    new Coordinate(0, 2),
+                    new Coordinate(0, 3)
+            ),
+            new Rotation(
+                    new Coordinate(0, 1),
+                    new Coordinate(1, 1),
+                    new Coordinate(2, 1),
+                    new Coordinate(3, 1)
+            )
+    )),
+
+    J("J", new Piece(
+            new Rotation(
+                    new Coordinate(0, 2),
+                    new Coordinate(1, 2),
+                    new Coordinate(2, 1),
+                    new Coordinate(2, 2)
+            ),
+            new Rotation(
+                    new Coordinate(0, 0),
+                    new Coordinate(0, 1),
+                    new Coordinate(0, 2),
+                    new Coordinate(1, 2)
+            ),
+            new Rotation(
+                    new Coordinate(0, 1),
+                    new Coordinate(0, 2),
+                    new Coordinate(1, 1),
+                    new Coordinate(2, 1)
+            ),
+            new Rotation(
+                    new Coordinate(0, 1),
+                    new Coordinate(1, 1),
+                    new Coordinate(1, 2),
+                    new Coordinate(1, 3)
+            )
+    )),
+
+    L("L", new Piece(
+            new Rotation(
+                    new Coordinate(0, 1),
+                    new Coordinate(1, 1),
+                    new Coordinate(2, 1),
+                    new Coordinate(2, 2)
+            ),
+            new Rotation(
+                    new Coordinate(0, 2),
+                    new Coordinate(1, 0),
+                    new Coordinate(1, 1),
+                    new Coordinate(1, 2)
+            ),
+            new Rotation(
+                    new Coordinate(0, 1),
+                    new Coordinate(0, 2),
+                    new Coordinate(1, 2),
+                    new Coordinate(2, 2)
+            ),
+            new Rotation(
+                    new Coordinate(0, 1),
+                    new Coordinate(0, 2),
+                    new Coordinate(0, 3),
+                    new Coordinate(1, 1)
+            )
+    )),
+
+    O("O", new Piece(
+            new Rotation(
+                    new Coordinate(0, 1),
+                    new Coordinate(0, 2),
+                    new Coordinate(1, 1),
+                    new Coordinate(1, 2)
+            )
+    )),
+
+    S("S", new Piece(
+            new Rotation(
+                    new Coordinate(0, 1),
+                    new Coordinate(0, 2),
+                    new Coordinate(1, 0),
+                    new Coordinate(1, 1)
+            ),
+
+            new Rotation(
+                    new Coordinate(0, 1),
+                    new Coordinate(1, 1),
+                    new Coordinate(1, 2),
+                    new Coordinate(2, 2)
+            )
+    )),
+
+    T("T", new Piece(
+            new Rotation(
+                    new Coordinate(0, 1),
+                    new Coordinate(1, 1),
+                    new Coordinate(1, 2),
+                    new Coordinate(2, 1)
+            ),
+            new Rotation(
+                    new Coordinate(0, 1),
+                    new Coordinate(1, 0),
+                    new Coordinate(1, 1),
+                    new Coordinate(1, 2)
+            ),
+            new Rotation(
+                    new Coordinate(0, 2),
+                    new Coordinate(1, 1),
+                    new Coordinate(1, 2),
+                    new Coordinate(2, 2)
+            ),
+            new Rotation(
+                    new Coordinate(0, 1),
+                    new Coordinate(0, 2),
+                    new Coordinate(0, 3),
+                    new Coordinate(1, 2)
+            )
+    )),
+
+    Z("Z", new Piece(
+            new Rotation(
+                    new Coordinate(0, 1),
+                    new Coordinate(0, 2),
+                    new Coordinate(1, 2),
+                    new Coordinate(1, 3)
+            ),
+            new Rotation(
+                    new Coordinate(0, 2),
+                    new Coordinate(1, 1),
+                    new Coordinate(1, 2),
+                    new Coordinate(2, 1)
+            )
+    ));
+
+    private final String label;
+    private final Piece piece;
+
+    Tetromino(String label, Piece piece) {
+        this.label = label;
+        this.piece = piece;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public Piece getPiece() {
+        return piece.copy();
+    }
+}
