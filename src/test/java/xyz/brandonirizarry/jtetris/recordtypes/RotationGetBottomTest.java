c3995409eaca613ba19d53364e22e336d76c9d96
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-09-18 19:18:06 -0400

    test: two simple tests for Rotation::getBottom

---
+++ src/test/java/xyz/brandonirizarry/jtetris/recordtypes/RotationGetBottomTest.java
@@ -0,0 +1,35 @@
+package xyz.brandonirizarry.jtetris.recordtypes;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import xyz.brandonirizarry.jtetris.Tetromino;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RotationGetBottomTest {
+    @Test
+    @DisplayName("Check bottom of L-piece")
+    void checkLPieceBottom() {
+        var lFirst = Tetromino.L.getPiece().getFirst();
+
+        assertEquals(
+                List.of(new Coordinate(2, 1), new Coordinate(2, 2)),
+                lFirst.getBottom()
+        );
+    }
+
+    @Test
+    @DisplayName("Check bottom of I-piece")
+    void checkIPieceBottom() {
+        var I = Tetromino.I.getPiece();
+        I.rotateCounterclockwise();
+        var iFirst = I.getFirst();
+
+        assertEquals(
+                List.of(new Coordinate(3, 1)),
+                iFirst.getBottom()
+        );
+    }
+}
