feeadbcae492f1297aee0acd1b428eb60ff84667
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-09-22 18:19:52 -0400

    refactor: move stencil definitions to src/test/resources
    This in particular means that we no longer have a 'main' method (our
    application isn't doing anything so far anyway; its API is only being
    incrementally designed and tested.)

--- src/test/java/xyz/brandonirizarry/jtetris/StencilDefinitionsTest.java
+++ src/test/java/xyz/brandonirizarry/jtetris/StencilDefinitionsTest.java
@@ -4,22 +4,91 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import xyz.brandonirizarry.jtetris.circularbuffer.Piece;
+import xyz.brandonirizarry.jtetris.recordtypes.Coordinate;
+import xyz.brandonirizarry.jtetris.recordtypes.Rotation;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StencilDefinitionsTest {
-    // Invoke 'main', so that the values of pieceMap are populated with
-    // rotations.
+    static Map<String, Piece> pieceMap = new HashMap<>(Map.of(
+            "I", new Piece(),
+            "J", new Piece(),
+            "L", new Piece(),
+            "S", new Piece(),
+            "O", new Piece(),
+            "T", new Piece(),
+            "Z", new Piece()
+    ));
+
+    private static Rotation getRotationFromFilepath(Path path) {
+        List<Coordinate> rotation = new ArrayList<>();
+
+        try (var scanner = new Scanner(path).useDelimiter("")) {
+            var rowIndex = 0;
+            var columnIndex = 0;
+
+            while (scanner.hasNext()) {
+                switch (scanner.next().charAt(0)) {
+                    case '\n' -> { continue; }
+                    case 'O' -> rotation.add(new Coordinate(rowIndex, columnIndex));
+                }
+
+                columnIndex++;
+
+                if (columnIndex == 4) {
+                    columnIndex = 0;
+                    rowIndex++;
+
+                    if (rowIndex == 4) break;
+                }
+            }
+        } catch (IOException e) {
+            System.out.printf("Couldn't create scanner for file '%s'%n", path);
+        }
+
+        return Rotation.fromList(rotation);
+    }
+
     @BeforeAll
-    static void populatePieceMap() {
-        JTetris.main(new String[]{});
+    public static void populatePieceMap() {
+        for (var pieceName : pieceMap.keySet()) {
+            var dir = JTetris.class.getClassLoader().getResource("stencilDefinitions/" + pieceName);
+            assert dir != null;
+
+            var rotations = pieceMap.get(pieceName);
+
+            try (var directoryContentsStream = Files.list(Paths.get(dir.toURI()))) {
+                directoryContentsStream
+                        .sorted()
+                        .forEach(path -> {
+                            var rotation = getRotationFromFilepath(path);
+                            rotations.add(rotation);
+                        });
+            } catch (URISyntaxException e) {
+                System.out.println("Directory resource invalid or not found");
+                System.out.println(e.getMessage());
+            } catch (IOException e) {
+                System.out.println("Something went wrong when opening the directory");
+                System.out.println(e.getMessage());
+            }
+
+            pieceMap.put(pieceName, rotations);
+        }
     }
 
     @ParameterizedTest
     @EnumSource
     @DisplayName("Stencil templates generate correct coordinates")
     void checkStencilCoordinates(Tetromino tetromino) {
-        var actualPiece = JTetris.pieceMap.get(tetromino.getLabel());
+        var actualPiece = pieceMap.get(tetromino.getLabel());
 
         assertEquals(tetromino.getPiece(), actualPiece, "Stencil generates incorrect coordinates");
     }
