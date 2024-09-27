30d389bb2f237680626537858744e4831596b129
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-09-26 20:19:05 -0400

    test: refactor scripted "games" into separate methods

--- src/test/java/xyz/brandonirizarry/jtetris/board/LineBreakTests.java
+++ src/test/java/xyz/brandonirizarry/jtetris/board/LineBreakTests.java
@@ -10,29 +10,123 @@ import static xyz.brandonirizarry.jtetris.board.BoardTestUtils.checkBoardAgainst
 public class LineBreakTests {
     private final Board board = new Board(21, 12);
 
+    private void sendIIO() {
+        board.introducePiece(Tetromino.I.getPiece().translate(0, 1));
+
+        for (var i = 0; i < 100; i++) {
+            board.moveDown();
+        }
+
+        board.introducePiece(Tetromino.I.getPiece().translate(0, 5));
+
+        for (var i = 0; i < 100; i++) {
+            board.moveDown();
+        }
+
+        board.introducePiece(Tetromino.O.getPiece().translate(0, 8));
+
+        for (var i = 0; i < 100; i++) {
+            board.moveDown();
+        }
+    }
+
+    private void send8O2IForTetris() {
+        /*
+                We introduce a "dream lineup" of pieces to achieve a tetris:
+                eight O's, followed by two I's
+             */
+
+        // Introduce two O's, stacked one on top of the other.
+        for (var i = 0; i < 2; i++) {
+            board.introducePiece(Tetromino.O.getPiece());
+
+            for (var j = 0; j < 100; j++) {
+                board.moveDown();
+            }
+        }
+
+        // Introduce two more stacked O's to the right.
+        for (var i = 0; i < 2; i++) {
+            board.introducePiece(Tetromino.O.getPiece().translate(0, 2));
+
+            for (var j = 0; j < 100; j++) {
+                board.moveDown();
+            }
+        }
+
+        // Introduce two more stacked O's to the right.
+        for (var i = 0; i < 2; i++) {
+            board.introducePiece(Tetromino.O.getPiece().translate(0, 4));
+
+            for (var j = 0; j < 100; j++) {
+                board.moveDown();
+            }
+        }
+
+        // Introduce two more stacked O's to the right.
+        for (var i = 0; i < 2; i++) {
+            board.introducePiece(Tetromino.O.getPiece().translate(0, 6));
+
+            for (var j = 0; j < 100; j++) {
+                board.moveDown();
+            }
+        }
+
+        // Finally, introduce two vertical I's to complete the tetris.
+        var iPiece = Tetromino.I.getPiece();
+
+        board.introducePiece(iPiece.translate(0, 8).rotateCounterclockwise());
+
+        for (var i = 0; i < 100; i++) {
+            board.moveDown();
+        }
+
+        board.introducePiece(iPiece.translate(0, 9).rotateClockwise());
+
+        for (var i = 0; i < 100; i++) {
+            board.moveDown();
+        }
+    }
+
+    private void sendJJTTForUnevenLineCompletion() {
+  /*
+                We introduce a janky lineup that no player would employ purposefully,
+                to demonstrate an uneven line break: send two J's, followed by two T's.
+             */
+        var jPiece = Tetromino.J.getPiece();
+        board.introducePiece(jPiece.translate(0, 1).rotateCounterclockwise());
+
+        for (var i = 0; i < 100; i++) {
+            board.moveDown();
+        }
+
+        board.introducePiece(jPiece.translate(0, 4).rotateCounterclockwise());
+
+        for (var i = 0; i < 100; i++) {
+            board.moveDown();
+        }
+
+        var tPiece = Tetromino.T.getPiece();
+        board.introducePiece(tPiece.translate(0, 6).rotateCounterclockwise(2));
+
+        for (var i = 0; i < 100; i++) {
+            board.moveDown();
+        }
+
+        board.introducePiece(tPiece.translate(0, 8));
+
+        for (var i = 0; i < 100; i++) {
+            board.moveDown();
+        }
+    }
+
     @Nested
     @DisplayName("Marking for deletion")
     class MarkingForDeletion {
         @Test
         @DisplayName("Marking for deletion after I, I, and O")
         void markingForDeletionAfterIIO() {
-            board.introducePiece(Tetromino.I.getPiece().translate(0, 1));
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
-
-            board.introducePiece(Tetromino.I.getPiece().translate(0, 5));
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
-
-            board.introducePiece(Tetromino.O.getPiece().translate(0, 8));
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
+            sendIIO();
 
             board.markFilledRowsForDeletion();
 
@@ -42,61 +136,7 @@ public class LineBreakTests {
         @Test
         @DisplayName("Marking for deletion after achieving a tetris")
         void markingForDeletionAfterTetris() {
-            /*
-                We introduce a "dream lineup" of pieces to achieve a tetris:
-                eight O's, followed by two I's
-             */
-
-            // Introduce two O's, stacked one on top of the other.
-            for (var i = 0; i < 2; i++) {
-                board.introducePiece(Tetromino.O.getPiece());
-
-                for (var j = 0; j < 100; j++) {
-                    board.moveDown();
-                }
-            }
-
-            // Introduce two more stacked O's to the right.
-            for (var i = 0; i < 2; i++) {
-                board.introducePiece(Tetromino.O.getPiece().translate(0, 2));
-
-                for (var j = 0; j < 100; j++) {
-                    board.moveDown();
-                }
-            }
-
-            // Introduce two more stacked O's to the right.
-            for (var i = 0; i < 2; i++) {
-                board.introducePiece(Tetromino.O.getPiece().translate(0, 4));
-
-                for (var j = 0; j < 100; j++) {
-                    board.moveDown();
-                }
-            }
-
-            // Introduce two more stacked O's to the right.
-            for (var i = 0; i < 2; i++) {
-                board.introducePiece(Tetromino.O.getPiece().translate(0, 6));
-
-                for (var j = 0; j < 100; j++) {
-                    board.moveDown();
-                }
-            }
-
-            // Finally, introduce two vertical I's to complete the tetris.
-            var iPiece = Tetromino.I.getPiece();
-
-            board.introducePiece(iPiece.translate(0, 8).rotateCounterclockwise());
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
-
-            board.introducePiece(iPiece.translate(0, 9).rotateClockwise());
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
+            send8O2IForTetris();
 
             board.markFilledRowsForDeletion();
 
@@ -106,35 +146,7 @@ public class LineBreakTests {
         @Test
         @DisplayName("Uneven line clear: no game is perfect :)")
         void unevenLineClear() {
-            /*
-                We introduce a janky lineup that no player would employ purposefully,
-                to demonstrate an uneven line break: send two J's, followed by two T's.
-             */
-            var jPiece = Tetromino.J.getPiece();
-            board.introducePiece(jPiece.translate(0, 1).rotateCounterclockwise());
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
-
-            board.introducePiece(jPiece.translate(0, 4).rotateCounterclockwise());
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
-
-            var tPiece = Tetromino.T.getPiece();
-            board.introducePiece(tPiece.translate(0, 6).rotateCounterclockwise(2));
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
-
-            board.introducePiece(tPiece.translate(0, 8));
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
+            sendJJTTForUnevenLineCompletion();
 
             board.markFilledRowsForDeletion();
 
@@ -148,35 +160,7 @@ public class LineBreakTests {
         @Test
         @DisplayName("Settling after uneven line clear")
         void settlingAfterUnevenLineClear() {
-             /*
-                We introduce a janky lineup that no player would employ purposefully,
-                to demonstrate an uneven line break: send two J's, followed by two T's.
-             */
-            var jPiece = Tetromino.J.getPiece();
-            board.introducePiece(jPiece.translate(0, 1).rotateCounterclockwise());
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
-
-            board.introducePiece(jPiece.translate(0, 4).rotateCounterclockwise());
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
-
-            var tPiece = Tetromino.T.getPiece();
-            board.introducePiece(tPiece.translate(0, 6).rotateCounterclockwise(2));
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
-
-            board.introducePiece(tPiece.translate(0, 8));
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
+            sendJJTTForUnevenLineCompletion();
 
             board.markFilledRowsForDeletion();
             board.eliminateClearedChars();
@@ -252,23 +236,7 @@ public class LineBreakTests {
         @Test
         @DisplayName("Settling after single completed row")
         void settlingAfterSingleCompletedRow() {
-            board.introducePiece(Tetromino.I.getPiece().translate(0, 1));
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
-
-            board.introducePiece(Tetromino.I.getPiece().translate(0, 5));
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
-
-            board.introducePiece(Tetromino.O.getPiece().translate(0, 8));
-
-            for (var i = 0; i < 100; i++) {
-                board.moveDown();
-            }
+            sendIIO();
 
             board.markFilledRowsForDeletion();
             board.eliminateClearedChars();
