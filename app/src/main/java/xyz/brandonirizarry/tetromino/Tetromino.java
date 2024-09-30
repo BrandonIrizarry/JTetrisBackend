27f45a2575634f3b83ffb268e28a734cae0abaf8
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-09-29 21:22:15 -0400

    feat: add complete set of tetromino definitions
    I'm pleased at how this is simpler than a point/coordinate-based
    approach.

--- app/src/main/java/xyz/brandonirizarry/tetromino/Tetromino.java
+++ app/src/main/java/xyz/brandonirizarry/tetromino/Tetromino.java
@@ -7,12 +7,28 @@ import java.util.List;
 import java.util.Objects;
 
 public class Tetromino {
-    private static ImmutableMap<String, List<Delta>> aliases = ImmutableMap.of(
-            "J1", List.of(new Delta(1, 0), new Delta(2, -1), new Delta(2, 0)),
-            "J2", List.of(new Delta(0, 1), new Delta(0, 2), new Delta(1, 2)),
-            "J3", List.of(new Delta(0, 1), new Delta(1, 0), new Delta(2, 0)),
-            "J4", List.of(new Delta(1, 0), new Delta(1, 1), new Delta(1, 2))
-    );
+    private static final ImmutableMap<String, List<Delta>> aliases =
+            new ImmutableMap.Builder<String, List<Delta>>()
+                    .put("I1", List.of(new Delta(0, 1), new Delta(0, 2), new Delta(0, 3)))
+                    .put("I2", List.of(new Delta(1, 0), new Delta(2, 0), new Delta(3, 0)))
+                    .put("J1", List.of(new Delta(1, 0), new Delta(2, -1), new Delta(2, 0)))
+                    .put("J2", List.of(new Delta(0, 1), new Delta(0, 2), new Delta(1, 2)))
+                    .put("J3", List.of(new Delta(0, 1), new Delta(1, 0), new Delta(2, 0)))
+                    .put("J4", List.of(new Delta(1, 0), new Delta(1, 1), new Delta(1, 2)))
+                    .put("L1", List.of(new Delta(1, 0), new Delta(2, 0), new Delta(2, 1)))
+                    .put("L2", List.of(new Delta(1, -2), new Delta(1, -1), new Delta(1, 0)))
+                    .put("L3", List.of(new Delta(0, 1), new Delta(1, 1), new Delta(2, 1)))
+                    .put("L4", List.of(new Delta(0, 1), new Delta(0, 2), new Delta(1, 0)))
+                    .put("O1", List.of(new Delta(0, 1), new Delta(1, 0), new Delta(1, 1)))
+                    .put("S1", List.of(new Delta(0, 1), new Delta(1, -1), new Delta(1, 0)))
+                    .put("S2", List.of(new Delta(1, 0), new Delta(1, 1), new Delta(2, 1)))
+                    .put("T1", List.of(new Delta(1, 0), new Delta(1, 1), new Delta(2, 0)))
+                    .put("T2", List.of(new Delta(1, -1), new Delta(1, 0), new Delta(1, 1)))
+                    .put("T3", List.of(new Delta(1, -1), new Delta(1, 0), new Delta(2, 0)))
+                    .put("T4", List.of(new Delta(0, 1), new Delta(0, 2), new Delta(1, 1)))
+                    .put("Z1", List.of(new Delta(0, 1), new Delta(1, 1), new Delta(1, 2)))
+                    .put("Z2", List.of(new Delta(1, -1), new Delta(1, 0), new Delta(2, -1)))
+                    .build();
 
     public static List<Delta> aliased(String alias) {
         return Objects.requireNonNull(aliases.get(alias));
