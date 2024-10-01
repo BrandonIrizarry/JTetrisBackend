a2f91f48470daa205038c7b0cc2286a084f234dd
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-09-30 20:11:22 -0400

    test: add test for empty 6x6 board

---
+++ app/src/test/java/xyz/brandonirizarry/tetrisboard/TetrisBoardTest.java
@@ -0,0 +1,16 @@
+package xyz.brandonirizarry.tetrisboard;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static xyz.brandonirizarry.TestUtils.checkBoardAgainstFileContents;
+
+public class TetrisBoardTest {
+    @Test
+    @DisplayName("Empty 6x6 board")
+    void confirmAppearanceOfEmpty6x6Board() {
+        var tetrisBoard = new TetrisBoard(6, 6);
+
+        checkBoardAgainstFileContents(tetrisBoard, "empty6x6board.txt");
+    }
+}
