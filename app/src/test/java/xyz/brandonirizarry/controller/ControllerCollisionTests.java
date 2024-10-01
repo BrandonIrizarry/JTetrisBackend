bad8b7b8957cbf2c8fe3631be2a1866f0de7c632
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-10-01 19:24:25 -0400

    test: introduce 'ControllerCollisionTests' test suite (with 'T')
    This starts out by testing the starting configuration when introducing
    the 'T' piece.

---
+++ app/src/test/java/xyz/brandonirizarry/controller/ControllerCollisionTests.java
@@ -0,0 +1,25 @@
+package xyz.brandonirizarry.controller;
+
+import org.junit.jupiter.api.*;
+import xyz.brandonirizarry.TestUtils;
+import xyz.brandonirizarry.tetrisboard.TetrisBoard;
+import xyz.brandonirizarry.tetromino.Tetromino;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ControllerCollisionTests {
+    private static Controller controller;
+    private static final TetrisBoard tetrisBoard = new TetrisBoard(6, 6);
+
+    @BeforeAll
+    public static void setup() {
+        controller = new Controller(tetrisBoard);
+        controller.startPiece(Tetromino.aliased("T1"));
+    }
+
+    @Test
+    @DisplayName("1. Starting configuration with T")
+    @Order(1)
+    void startingConfigurationWithT() {
+        TestUtils.checkBoardAgainstFileContents(tetrisBoard, "controllerCollisionTests/1_startT.txt");
+    }
+}
