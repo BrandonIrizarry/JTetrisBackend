f9d7d00a410744d7d7a838f1f116b39bfb486af2
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-10-01 21:18:25 -0400

    test: garbage is collidable when moving down

---
+++ app/src/test/java/xyz/brandonirizarry/controller/ControllerGarbageTests.java
@@ -0,0 +1,36 @@
+package xyz.brandonirizarry.controller;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import xyz.brandonirizarry.TestUtils;
+import xyz.brandonirizarry.tetrisboard.TetrisBoard;
+import xyz.brandonirizarry.tetromino.Tetromino;
+
+public class ControllerGarbageTests {
+    private static Controller controller;
+    private static final TetrisBoard tetrisBoard = new TetrisBoard(6, 8);
+
+    @BeforeAll
+    public static void setup() {
+        controller = new Controller(tetrisBoard);
+    }
+
+    @Test
+    @DisplayName("L frozen on top of L-garbage")
+    void sendLOnTopOfL() {
+        controller.startPiece(Tetromino.aliased("I1"));
+
+        for (var i = 0; i < 20; i++) {
+            controller.moveDown();
+        }
+
+        controller.startPiece(Tetromino.aliased("I1"));
+
+        for (var i = 0; i < 20; i++) {
+            controller.moveDown();
+        }
+
+        TestUtils.checkBoardAgainstFileContents(tetrisBoard, "controllerGarbageTests/LonL.txt");
+    }
+}
