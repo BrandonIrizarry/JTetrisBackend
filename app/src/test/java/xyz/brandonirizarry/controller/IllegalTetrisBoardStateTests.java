3f9a835d36bb58cb8c15bc0644df3e12202bd539
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-10-02 19:55:12 -0400

    test: premature 'startPiece' call throws

---
+++ app/src/test/java/xyz/brandonirizarry/controller/IllegalTetrisBoardStateTests.java
@@ -0,0 +1,29 @@
+package xyz.brandonirizarry.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import xyz.brandonirizarry.tetrisboard.TetrisBoard;
+import xyz.brandonirizarry.tetromino.Tetromino;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IllegalTetrisBoardStateTests {
+    private TetrisBoard tetrisBoard;
+    private Controller controller;
+
+    @BeforeEach
+    void setupController() {
+        tetrisBoard = new TetrisBoard(6, 8);
+        controller = new Controller(tetrisBoard);
+    }
+
+    @Test
+    @DisplayName("Prematurely starting another piece throws an exception")
+    void prematureStartPiece() {
+        controller.startPiece(Tetromino.aliased("O1"));
+        controller.moveLeft();
+        controller.moveLeft();
+        assertThrows(IllegalStateException.class, () -> controller.startPiece(Tetromino.aliased("O1")));
+    }
+}
