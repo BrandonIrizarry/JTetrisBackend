46782d08e12635e0030e0542690d5c0bd5564c78
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-10-02 18:00:04 -0400

    test: demonstrate shifting effect caused by rotation
    This test is a variant of the one we just added, only it highlights
    the fact that rotations use an "origin delta" to obtain the correct
    positioning of the next rotation.

--- app/src/test/java/xyz/brandonirizarry/controller/ControllerGarbageCollisionTests.java
+++ app/src/test/java/xyz/brandonirizarry/controller/ControllerGarbageCollisionTests.java
@@ -48,4 +48,34 @@ public class ControllerGarbageCollisionTests {
 
         TestUtils.checkBoardAgainstFileContents(tetrisBoard, "controllerGarbageCollisionTests/OtoIFromLeft.txt");
     }
+
+    @Test
+    @DisplayName("Left-collide O against frozen I (I1)")
+    void leftCollideOAgainstFrozenI_I1() {
+        var tetrisBoard = new TetrisBoard(6, 8);
+        var controller = new Controller(tetrisBoard);
+
+        // This test is included to illustrate a subtlety that arises
+        // based on which rotation you start out with. Rotating
+        // effectively changes the location of a tetromino's origin,
+        // which doesn't happen when you simply start out with the
+        // rotated variant, as in the previous test. And so in this
+        // test we have to invoke 'moveLeft' to undo the (expected)
+        // shift to the right incurred by the rotation.
+        controller.startPiece(Tetromino.aliased("I1"));
+
+        controller.rotateCounterclockwise();
+        controller.moveLeft();
+
+        for (var i = 0; i < 3; i++) {
+            controller.moveDown();
+        }
+
+        controller.startPiece(Tetromino.aliased("O1"));
+        controller.moveRight();
+        controller.moveDown();
+        controller.moveLeft();
+
+        TestUtils.checkBoardAgainstFileContents(tetrisBoard, "controllerGarbageCollisionTests/OtoIFromLeft.txt");
+    }
 }
