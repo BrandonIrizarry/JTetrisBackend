eb520d4d681a75357ed4322014d3ba36c399cfe7
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-10-03 21:35:53 -0400

    test: introduce first complex line-clearing test

--- app/src/test/java/xyz/brandonirizarry/controller/LineClearingTests.java
+++ app/src/test/java/xyz/brandonirizarry/controller/LineClearingTests.java
@@ -13,11 +13,19 @@ import java.util.List;
 public class LineClearingTests {
     private TetrisBoard tetrisBoard;
     private Controller controller;
+
+    // Let's define fields for our pieces up front, to make the individual tests
+    // themselves easier to write.
     private final List<Delta> I1 = Tetromino.aliased("I1");
+    private final List<Delta> L1 = Tetromino.aliased("L1");
+    private final List<Delta> T1 = Tetromino.aliased("T1");
+    private final List<Delta> O1 = Tetromino.aliased("O1");
+    private final List<Delta> S1 = Tetromino.aliased("S1");
+    private final List<Delta> J1 = Tetromino.aliased("J1");
 
     @BeforeEach
     void setupController() {
-        tetrisBoard = new TetrisBoard(6, 8);
+        tetrisBoard = new TetrisBoard(10, 8);
         controller = new Controller(tetrisBoard);
     }
 
@@ -35,5 +43,47 @@ public class LineClearingTests {
         controller.hardDrop();
 
         TestUtils.checkBoardAgainstFileContents(tetrisBoard, "lineClearingTests/empty6x8Board.txt");
+    }
+
+    @Test
+    @DisplayName("Collapse involving a variety of pieces")
+    void complexCollapseVarietyOfPieces() {
+        controller.startPiece(O1);
+        controller.moveLeft();
+        controller.moveLeft();
+        controller.moveLeft();
+        controller.hardDrop();
+
+        controller.startPiece(T1);
+        controller.rotateCounterclockwise();
+        controller.moveLeft();
+        controller.moveLeft();
+        controller.hardDrop();
+
+        controller.startPiece(S1);
+        controller.rotateCounterclockwise();
+        controller.hardDrop();
+
+        controller.startPiece(J1);
+        controller.rotateCounterclockwise();
+        controller.moveLeft();
+        controller.hardDrop();
+
+        controller.startPiece(L1);
+        controller.moveRight();
+        controller.rotateCounterclockwise();
+        controller.rotateCounterclockwise();
+        controller.hardDrop();
+
+        controller.startPiece(J1);
+        controller.rotateCounterclockwise();
+        controller.rotateCounterclockwise();
+        controller.moveRight();
+        controller.moveRight();
+        controller.moveRight();
+        controller.moveRight();
+        controller.hardDrop();
+
+        TestUtils.checkBoardAgainstFileContents(tetrisBoard, "lineClearingTests/complexCollapse1.txt");
     }
 }
