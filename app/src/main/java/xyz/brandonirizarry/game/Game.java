c253143467f1c51057ab9f09f56f9178b785dc13
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-10-17 12:29:07 -0400

    refactor: canonicalize Game class in preparation for Maven packaging

--- app/src/main/java/xyz/brandonirizarry/game/Game.java
+++ app/src/main/java/xyz/brandonirizarry/game/Game.java
@@ -1,57 +1,42 @@
 package xyz.brandonirizarry.game;
 
-import com.google.common.collect.ImmutableMap;
 import xyz.brandonirizarry.controller.Controller;
 import xyz.brandonirizarry.tetrisboard.TetrisBoard;
-import xyz.brandonirizarry.tetromino.Tetromino;
-
-import java.util.Objects;
-import java.util.Random;
-import java.util.Scanner;
-import java.util.function.Consumer;
 
 public class Game {
     private final TetrisBoard tetrisBoard;
     private final Controller controller;
 
-    private static final ImmutableMap<String, Consumer<Controller>> commandMap =
-            new ImmutableMap.Builder<String, Consumer<Controller>>()
-                    .put("pass", (controller) -> { })
-                    .put("left", Controller::moveLeft)
-                    .put("right", Controller::moveRight)
-                    .put("ccw", Controller::rotateCounterclockwise)
-                    .put("cw", Controller::rotateClockwise)
-                    .put("slam", Controller::hardDrop)
-                    .put("next", (controller) -> {
-                        var tetrominoAliases = Tetromino.aliases.keySet().asList();
-                        var tetromino = Tetromino.aliased(tetrominoAliases.get(new Random().nextInt(tetrominoAliases.size())));
-                        controller.startPiece(tetromino);
-                    })
-                    .build();
-
-    public Game() {
-        tetrisBoard = new TetrisBoard(20, 10);
-        controller = new Controller(tetrisBoard);
-
-        start();
-    }
-
     public Game(int numRows, int numColumns) {
-        tetrisBoard = new TetrisBoard(numRows, numColumns);
-        controller = new Controller(tetrisBoard);
-
-        start();
+        this.tetrisBoard = new TetrisBoard(numRows, numColumns);
+        this.controller = new Controller(this.tetrisBoard);
     }
 
-    public void start() {
-        var scanner = new Scanner(System.in);
+    public void moveDown() {
+        controller.moveDown();
+    }
 
-        while (scanner.hasNextLine()) {
-            var input = scanner.nextLine().trim();
-            var command = Objects.requireNonNullElse(commandMap.get(input), (controller) -> System.out.println(commandMap.keySet()));
-            command.accept(controller);
-            controller.moveDown();
-            System.out.println(tetrisBoard);
-        }
+    public void moveLeft() {
+        controller.moveLeft();
+    }
+
+    public void moveRight() {
+        controller.moveRight();
+    }
+
+    public void rotateCounterclockwise() {
+        controller.rotateCounterclockwise();
+    }
+
+    public void rotateClockwise() {
+        controller.rotateClockwise();
+    }
+
+    public void hardDrop() {
+        controller.hardDrop();
+    }
+
+    public Cell[][] export() {
+        return this.tetrisBoard.export();
     }
 }
