e82c5751a1d7112065f70a2c8dec9497a5e7e6d2
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-10-18 19:46:54 -0400

    feat: add 'sendNextPiece' to JTetris Game API

--- app/src/main/java/xyz/brandonirizarry/game/Game.java
+++ app/src/main/java/xyz/brandonirizarry/game/Game.java
@@ -2,6 +2,9 @@ package xyz.brandonirizarry.game;
 
 import xyz.brandonirizarry.controller.Controller;
 import xyz.brandonirizarry.tetrisboard.TetrisBoard;
+import xyz.brandonirizarry.tetromino.Tetromino;
+
+import java.util.Random;
 
 public class Game {
     private final TetrisBoard tetrisBoard;
@@ -34,6 +37,12 @@ public class Game {
 
     public void hardDrop() {
         controller.hardDrop();
+    }
+
+    public void sendNextPiece() {
+        var tetrominoAliases = Tetromino.aliases.keySet().asList();
+        var tetromino = Tetromino.aliased(tetrominoAliases.get(new Random().nextInt(tetrominoAliases.size())));
+        controller.startPiece(tetromino);
     }
 
     public Cell[][] export() {
